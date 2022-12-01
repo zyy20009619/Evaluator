c61442aa33ee3481628285e14252fdbf28fa2653
zyy <2046517495@qq.com>
2022-12-01 11:10:21 +0800

    增加模块级模型

---
+++ module_measurement/module_metrics.py
@@ -0,0 +1,43 @@
+class ModuleMetric:
+    def __init__(self, name):
+        self.name = name
+        self.scoh = -1
+        self.scop = -1
+        self.odd = -1
+        self.idd = -1
+        self.spread = -1
+        self.focus = -1
+        self.icf = -1
+        self.ecf = -1
+        self.rei = -1
+        self.DSM = -1
+
+    def set_scoh(self):
+        pass
+
+    def set_scop(self):
+        pass
+
+    def set_odd(self):
+        pass
+
+    def set_idd(self):
+        pass
+
+    def set_spread(self):
+        pass
+
+    def set_focus(self):
+        pass
+
+    def set_icf(self):
+        pass
+
+    def set_ecf(self):
+        pass
+
+    def set_rei(self):
+        pass
+
+    def set_DSM(self):
+        pass
